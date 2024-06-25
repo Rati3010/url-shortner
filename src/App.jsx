e3d393b3447413de8e06307cd704788e5d5694ab
Rati3010 <mratikanta1997@gmail.com>
2024-06-25 09:17:28 +0530

    done with the header and folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,48 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
+import Applayout from './layouts/app.layout'
+import LandingPage from './pages/Landing'
+import Dashboard from './pages/Dashboard'
+import Auth from './pages/Auth'
+import Redirect from './pages/Redirect'
+import Link from './pages/Link'
 
+const router = createBrowserRouter([
+  {
+    element : <Applayout/>,
+    children:[
+      {
+        path:'/',
+        element:<LandingPage/>
+      },
+      {
+        path:'/dashboard',
+        element:<Dashboard/>
+      },
+      {
+        path:'/auth',
+        element:<Auth/>
+      },
+      {
+        path:'/:id',
+        element:<Redirect/>
+      },
+      {
+        path:'/link/:id',
+        element:<Link/>
+      },
+      {
+        path:'/',
+        element:<LandingPage/>
+      },
+    ]
+  }
+])
 function App() {
 
   return (
     <>
-      <div className='text-2xl'>
-        Happy Coding
-      </div>
+      <RouterProvider router={router}/>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import { Link, useNavigate } from 'react-router-dom';
+import { Button } from './ui/button';
+import { Avatar, AvatarFallback, AvatarImage } from '@radix-ui/react-avatar';
+import { LinkIcon, LogOut } from 'lucide-react';
+
+const Header = () => {
+  const navigate = useNavigate();
+  const user = true;
+  return (
+    <nav className="py-4 flex justify-between items-center">
+      <Link to={'/'}>
+        <img src="/arc.png" alt="Rat Arc logo" className="h-16" />
+      </Link>
+      <div>
+        {!user ? (
+          <Button onClick={() => navigate('/auth')}>Login</Button>
+        ) : (
+          <DropdownMenu>
+            <DropdownMenuTrigger className="w-10 rounded-full overflow-hidden">
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>RA</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>RAT ARC</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <LinkIcon className='mr-2 h-4 w-4'/>
+                <span>My Link</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem className="text-red-400">
+                <LogOut className='mr-2 h-4 w-4'/>
+                  <span>Logout</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
